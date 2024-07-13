cf50a6fb549628029b09dae02a587dbb026ad0cb
RUHULAMIN2024 <ruhulamin15jun1999@gmail.com>
2024-07-13 15:57:12 +0600

    mongobd conected

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.rth5hqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.KEY}:${process.env.KEY}@cluster0.rth5hqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
